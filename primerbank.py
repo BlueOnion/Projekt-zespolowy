ddefd6b7a4343f8fef1c6d8d5b240aec447a5f81
Łukasz Jackiewicz <ukasz Jackiewicz>
2017-01-13 21:06:09 +0100

    Poprawki

--- primerbank.py
+++ primerbank.py
@@ -80,26 +80,25 @@ def add_primer():
 # edytowanie primera
 @app.route('/edit_primer/<int:x>', methods=['GET', 'POST'])
 def edit_primer(x):
-    # odmowa dostępu w przypadku użytkownika niezalogowanego
-    if 'email' not in session:
-        abort(401)
-    # odmowa dostępu w przypadku użytkownika zalogowanego nie będącego właścicielem
-    primer = query_db('select * from primers where pid=?', [x,], one=True)
-    if primer is None:
-        abort(404)
-    elif session['email'] != primer['owner']:
-        abort(401)
+	# odmowa dostępu w przypadku użytkownika niezalogowanego
+	if 'email' not in session:
+		abort(401)
+	# odmowa dostępu w przypadku użytkownika zalogowanego nie będącego właścicielem
+	primer = query_db('select * from primers where pid=?', [x,], one=True)
+	if primer is None:
+		abort(404)
+	elif session['email'] != primer['owner']:
+		abort(401)
     # właściwa funkcja
     primers = query_db('select * from primers where pid=? and owner=?', [x, session['email']])
     if request.method == 'POST':
 		db = get_db()
-        db.execute('update primers set pname=?, psequence=? where pid=?', [request.form['pname'], request.form['psequence'], x])
-        db.execute('update primers set pname=?, ptype=?, psequence=?, nt=?, temp_gen=?, temp_calc=?, oligo_date=?, buffer=?, prep_date=?, gene_name=?, gb_acc_no=?, ncbi_id=?, ncbi_pa=?, gene_comment=?, genus=?, species=?, gene_desc=?, plasmid_name=?, seq_desc=?, seq_list=?, matrix_prep=?, cycles=?, final_conc=?, info=?, pmid=?, order_date=?, firm=?, facture_no=?, keywords=?, status=? where pid=?',
-            [request.form['pname'], request.form['ptype'], request.form['psequence'], request.form['nt'], request.form['temp_gen'], request.form['temp_calc'], request.form['oligo_date'], request.form['buffer'], request.form['prep_date'], request.form['gene_name'], request.form['gb_acc_no'], request.form['ncbi_id'], request.form['ncbi_pa'], request.form['gene_comment'], request.form['genus'], request.form['species'], request.form['gene_desc'], request.form['plasmid_name'], request.form['seq_desc'], request.form['seq_list'], request.form['matrix_prep'], request.form['cycles'], request.form['final_conc'], request.form['info'], request.form['pmid'], request.form['order_date'], request.form['firm'], request.form['facture_no'], request.form['keywords'], request.form['status'], x])
-        db.commit()
-        flash('Wpis został pomyślnie edytowany', 'message')
-        return(redirect(url_for('my_primers')))
-    return render_template('edit_primer.html', value=x, primers=primers)
+		db.execute('update primers set pname=?, ptype=?, psequence=?, nt=?, temp_gen=?, temp_calc=?, oligo_date=?, buffer=?, prep_date=?, gene_name=?, gb_acc_no=?, ncbi_id=?, ncbi_pa=?, gene_comment=?, genus=?, species=?, gene_desc=?, plasmid_name=?, seq_desc=?, seq_list=?, matrix_prep=?, cycles=?, final_conc=?, info=?, pmid=?, order_date=?, firm=?, facture_no=?, keywords=?, status=? where pid=?',
+					[request.form['pname'], request.form['ptype'], request.form['psequence'], request.form['nt'], request.form['temp_gen'], request.form['temp_calc'], request.form['oligo_date'], request.form['buffer'], request.form['prep_date'], request.form['gene_name'], request.form['gb_acc_no'], request.form['ncbi_id'], request.form['ncbi_pa'], request.form['gene_comment'], request.form['genus'], request.form['species'], request.form['gene_desc'], request.form['plasmid_name'], request.form['seq_desc'], request.form['seq_list'], request.form['matrix_prep'], request.form['cycles'], request.form['final_conc'], request.form['info'], request.form['pmid'], request.form['order_date'], request.form['firm'], request.form['facture_no'], request.form['keywords'], request.form['status'], x])
+		db.commit()
+		flash('Wpis został pomyślnie edytowany', 'message')
+		return(redirect(url_for('my_primers')))
+	return render_template('edit_primer.html', value=x, primers=primers)
 
 # usuwanie primera
 @app.route('/del_primer/<int:x>')
